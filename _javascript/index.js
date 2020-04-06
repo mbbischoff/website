1caadfcab73ad2a84d3907ed544090bea946f554
Matthew Bischoff <mb@matthewbischoff.com>
2020-04-05 21:48:41 -0400

    Updated Index.Js and 3 other files

--- _javascript/index.js
+++ _javascript/index.js
@@ -84,24 +84,24 @@ fetch("/feed.json").then(response => response.json()).then((data) => {
 
 initializeMobileMenu();
 
-// Navbar scroll into view on mobile
-const nav = document.querySelector("#categories>div");
-const cssTransitionTime = 800;
-
-if (window.innerWidth < 700) {
-  window.onload = () => {
-    nav.style.left = "-20px";
-    setTimeout(() => {
-      nav.style.left = "0px";
-      setTimeout(() => {
-        nav.style.width = "initial";
-        nav.parentElement.style.overflowX = "visible";
-      }, cssTransitionTime);
-    }, cssTransitionTime);
-  }
-} else {
-  nav.style.width = "initial";
-  nav.parentElement.style.overflowX = "visible";
-}
-
-nav.addEventListener("scroll", retractYearList);
+// Navbar scroll into view on mobile. Commented out for now.
+// const nav = document.querySelector("#categories>div");
+// const cssTransitionTime = 800;
+// 
+// if (window.innerWidth < 700) {
+//   window.onload = () => {
+//     nav.style.left = "-20px";
+//     setTimeout(() => {
+//       nav.style.left = "0px";
+//       setTimeout(() => {
+//         nav.style.width = "initial";
+//         nav.parentElement.style.overflowX = "visible";
+//       }, cssTransitionTime);
+//     }, cssTransitionTime);
+//   }
+// } else {
+//   nav.style.width = "initial";
+//   nav.parentElement.style.overflowX = "visible";
+// }
+// 
+// nav.addEventListener("scroll", retractYearList);

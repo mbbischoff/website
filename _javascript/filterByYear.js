92f8a3b367eccd09c19295f738dab11c04b75895
Martin-Alexander <martingianna@gmail.com>
2020-09-22 06:15:03 -0400

    new categories (years), changed the way the year dropdown works, and removed unneccessary JS

--- _javascript/filterByYear.js
+++ _javascript/filterByYear.js
@@ -1,70 +1,7 @@
-import { setPosts } from "./setPosts";
-import { getYear, setYear, removeCategory } from "./index";
-
 const latestTag = document.querySelector("#latest");
 const yearsList = document.querySelector("#years");
-let uniqueYears;
 
-export const initializeFilterByYear = (data, postList) => {
-  const footer = document.querySelector("footer");
-  const main = document.querySelector("main");
-  const categoryButtons = document.querySelectorAll(".category");
-
-  if ([latestTag, yearsList].some(element => element === null)) { return; }
-
-  const currentYear = getYear();
-
-  if (currentYear) {
-    latestTag.innerText = currentYear;
-    latestTag.classList.add("blue-highlight")
-  }
-
-  uniqueYears = Array.from(new Set(data.items.map(item => new Date(item.date_published).getFullYear())));
-  uniqueYears.forEach((year) =>  {
-    yearsList.insertAdjacentHTML("beforeend", `
-      <li class="button-round">
-        <a class="year" data-year="${year}" href="/?year=${year}">
-          ${year}
-        </a>
-      </li>
-    `);
-  });
-
-  document.querySelectorAll(".year").forEach((yearButton) => {
-    yearButton.addEventListener("click", (event) => {
-      const year = parseInt(yearButton.dataset.year);
-      setYear(year);
-      removeCategory();
-
-      if (!postList) { return; }
-      event.preventDefault();
-
-      footer.style.opacity = "0";
-      main.style.opacity = "0";
-
-      setTimeout(() => {
-        setPosts(data, postList);
-        setTimeout(() => {
-          main.style.opacity = "1";
-          footer.style.opacity = "1";
-        }, 300);
-      }, 300);
-
-      const url = new URL(location.href);
-      url.pathname = "";
-      url.search = `year=${year}`;
-
-      latestTag.innerText = year;
-
-      document.title = `${year} · Matthew Bischoff`;
-      window.history.replaceState(document.title, document.title, url.toString());
-
-      yearsList.style.height = "0px";
-      yearsList.classList.toggle("show");
-      categoryButtons.forEach(btn => btn.classList.remove("blue-highlight"));
-    });
-  });
-
+export const initializeFilterByYear = () => {
   latestTag.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -77,7 +14,7 @@ export const initializeFilterByYear = (data, postList) => {
 }
 
 export const extendYearList = () => {
-  yearsList.style.height = `${uniqueYears.length * 38}px`;
+  yearsList.style.height = `${7 * 38}px`;
   latestTag.classList.add("blue-highlight");
   yearsList.classList.add("show");
 }
--- _javascript/utils.js
+++
@@ -1,3 +0,0 @@
-export const capitalize = (string) => {
-  return string[0].toUpperCase() + string.substring(1);
-}
